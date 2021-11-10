ed797e003292dd9f7f13db38515bb6f63c13fba0
aravindsoundar <aravindsoundar2k2@gmail.com>
2021-11-10 12:10:06 +0400

    refactor data download

--- jupyterworkflow1/data.py
+++ jupyterworkflow1/data.py
@@ -8,7 +8,13 @@ FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=
 def get_fremont_data(filename='Fremont.csv', url = FREMONT_URL, force_download=False):
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv('Fremont.csv', index_col = 'Date', parse_dates=True)
+    data = pd.read_csv('Fremont.csv', index_col = 'Date')
+
+try:
+    data.index = pd.to_datetime(data.index, format = '%m/%d/%Y %H:%M:%S %p')
+except TypeError:
+    data.index = pd.to_datetime(date.index)
+
     data.columns =['total','east','west']
     return data
     
